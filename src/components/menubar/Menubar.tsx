6118adc397798236c355e8a8bfa866e20185c64f
Max Dyrø Finanger <max.d.finanger@gmail.com>
2024-07-07 17:20:44 +0200

    Update Menubar.tsx

--- src/components/menubar/Menubar.tsx
+++ src/components/menubar/Menubar.tsx
@@ -2,15 +2,14 @@ import React from "react";
 
 function Menubar() {
   return (
-      <>
-          <nav className="bg-dark text-white p-1 w-100 d-flex justify-content-left text-left ps-4">
-              <button type="button" className="btn text-white">
-                  <h1>Babybet</h1>
-              </button>
-              <button type="button" className="btn text-white">
-              </button>
-          </nav>
-      </>
+    <>
+      <nav className="bg-dark text-white p-1 w-100 d-flex justify-content-left text-left ps-4">
+        <button type="button" className="btn text-white">
+          <h1>Babybet</h1>
+        </button>
+        <button type="button" className="btn text-white"></button>
+      </nav>
+    </>
   );
 }
 
